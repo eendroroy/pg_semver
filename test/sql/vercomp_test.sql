708c09c30e49c0509e6df30eaf42666fb83ccd2b
indrajit <eendroroy@gmail.com>
2018-08-18 12:32:02 +0600

    plugin tsts

--- test/sql/vercomp_test.sql
+++ test/sql/vercomp_test.sql
@@ -1,1 +1,7 @@
 LOAD 'vercomp';
+
+CREATE TABLE ver_test(version VER);
+
+INSERT INTO ver_test VALUES ('0.0.0'), ('1.0.0'), ('2.0.0-alpha'), ('2.0.0-beta0'), ('2.0.0-beta1'), ('2.0.0-rc1'), ('3.0.0');
+
+SELECT * FROM ver_test;
