809966675220313b20647d18aa85ed566cf49d2b
indrajit <eendroroy@gmail.com>
2018-08-18 13:36:05 +0600

    update rw tests

--- test/sql/vercomp_test_rw.sql
+++ test/sql/vercomp_test_rw.sql
@@ -5,3 +5,5 @@ CREATE TABLE ver_test(version VER);
 INSERT INTO ver_test VALUES ('0.0.0'), ('1.0.0'), ('2.0.0-alpha'), ('2.0.0-beta0'), ('2.0.0-beta1'), ('2.0.0-rc1'), ('3.0.0');
 
 SELECT * FROM ver_test;
+
+DROP TABLE IF EXISTS ver_test;
