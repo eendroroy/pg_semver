aa937ff2a946d09e65fc2bd8688a120103bc5201
indrajit <eendroroy@gmail.com>
2018-08-19 00:06:16 +0600

    error messages

--- vercomp.c
+++ vercomp.c
@@ -5,6 +5,8 @@
 #include "semver.c/semver.h"
 #include "semver.c/semver.c"
 
+#define ERR_MSG_INVALID_PARAMETER_FMT "Invalid Version '%s'. Did you mean '%s'"
+
 PG_MODULE_MAGIC;
 
 bool ver_op(char* v1, char* v2, char* operator);
@@ -24,21 +26,26 @@ bool ver_op(char* v1, char* v2, char* operator) {
 
 PG_FUNCTION_INFO_V1(ver_in);
 Datum ver_in(PG_FUNCTION_ARGS) {
+    char vc[1000];
     char* v1 = PG_GETARG_CSTRING(0);
     text* t = cstring_to_text(v1);
     if (!semver_is_valid(v1)) {
-        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("Invalid Version %s.", v1)));
+        strcpy(vc, v1);
+        semver_clean(vc);
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg(ERR_MSG_INVALID_PARAMETER_FMT , v1, vc)));
     }
     PG_RETURN_CSTRING(t);
 }
 
 PG_FUNCTION_INFO_V1(ver_out);
 Datum ver_out(PG_FUNCTION_ARGS) {
-    char *v1 ;
+    char *v1, vc[1000];
     text *t = PG_GETARG_TEXT_PP(0);
     v1 = text_to_cstring(t);
     if (!semver_is_valid(v1)) {
-        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("Invalid Version %s.", v1)));
+        strcpy(vc, v1);
+        semver_clean(vc);
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg(ERR_MSG_INVALID_PARAMETER_FMT , v1, vc)));
     }
     PG_RETURN_CSTRING(v1);
 }
@@ -46,7 +53,7 @@ Datum ver_out(PG_FUNCTION_ARGS) {
 PG_FUNCTION_INFO_V1(ver_cmp);
 Datum ver_cmp(PG_FUNCTION_ARGS) {
     int resolution;
-    char *v1, *v2;
+    char *v1, *v2, vc1[1000], vc2[1000];
     semver_t v_1 = {};
     semver_t v_2 = {};
     text *t1 = PG_GETARG_TEXT_PP(0);
@@ -56,11 +63,15 @@ Datum ver_cmp(PG_FUNCTION_ARGS) {
     v2 = text_to_cstring(t2);
 
     if (!semver_is_valid(v1)) {
-        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("Invalid Version %s.", v1)));
+        strcpy(vc1, v1);
+        semver_clean(vc1);
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg(ERR_MSG_INVALID_PARAMETER_FMT , v1, vc1)));
     }
 
     if (!semver_is_valid(v2)) {
-        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("Invalid Version %s.", v2)));
+        strcpy(vc2, v2);
+        semver_clean(vc2);
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg(ERR_MSG_INVALID_PARAMETER_FMT , v2, vc2)));
     }
 
     semver_parse(v1, &v_1);
