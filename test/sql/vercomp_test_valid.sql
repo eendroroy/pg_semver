403bd9e9d362439feb6906c310a4c09a615d4f22
indrajit <eendroroy@gmail.com>
2018-08-20 21:22:32 +0600

    update tests

--- test/sql/vercomp_test_valid.sql
+++ test/sql/vercomp_test_valid.sql
@@ -7,21 +7,9 @@ INSERT INTO versions VALUES ('0.0');
 INSERT INTO versions VALUES ('0.1');
 INSERT INTO versions VALUES ('0.0.0');
 INSERT INTO versions VALUES ('0.0.1');
-INSERT INTO versions VALUES ('0.0.1-a');
-INSERT INTO versions VALUES ('0.0.1-alpha');
-INSERT INTO versions VALUES ('0.0.1-a.0');
-INSERT INTO versions VALUES ('0.0.1-a.0.1');
-INSERT INTO versions VALUES ('0.0.1-alpha.0');
-INSERT INTO versions VALUES ('0.0.1-alpha.0.1');
-INSERT INTO versions VALUES ('0.0.1-b');
-INSERT INTO versions VALUES ('0.0.1-beta');
-INSERT INTO versions VALUES ('0.0.1-b.0');
-INSERT INTO versions VALUES ('0.0.1-b.0.1');
-INSERT INTO versions VALUES ('0.0.1-beta.0');
-INSERT INTO versions VALUES ('0.0.1-beta.0.1');
-INSERT INTO versions VALUES ('0.0.1-rc');
-INSERT INTO versions VALUES ('0.0.1-rc.0');
-INSERT INTO versions VALUES ('0.0.1-rc.0.1');
+INSERT INTO versions VALUES ('0.0.1-alpha1');
+INSERT INTO versions VALUES ('0.0.1-beta1');
+INSERT INTO versions VALUES ('0.0.1-rc1');
 
 
 
@@ -30,9 +18,7 @@ INSERT INTO versions VALUES ('@0.0');
 INSERT INTO versions VALUES ('0.#1');
 INSERT INTO versions VALUES ('0.0.%0');
 INSERT INTO versions VALUES ('0.0.1');
-INSERT INTO versions VALUES ('0.0^.1-a');
 INSERT INTO versions VALUES ('0.&0.1-alpha');
-INSERT INTO versions VALUES ('0.0.*1-a.0');
 
 SELECT * FROM versions;
 
