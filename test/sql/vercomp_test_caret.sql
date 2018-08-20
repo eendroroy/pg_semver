cada9bfa49c839e69a7518ea80503bdf67f16247
indrajit <eendroroy@gmail.com>
2018-08-20 23:21:50 +0600

    update tests

--- test/sql/vercomp_test_caret.sql
+++ test/sql/vercomp_test_caret.sql
@@ -8,12 +8,11 @@ INSERT INTO versions VALUES ('0.0.1'), ('0.0.4'), ('0.0.5'), ('0.0.10'),
                             ('1.5.1'), ('2.3.4'), ('6.5.5'), ('10.10.10');
 
 SELECT * FROM versions WHERE version ^  '1.1.1';
-SELECT * FROM versions WHERE version !^ '1.1.1';
-
 SELECT * FROM versions WHERE version ^  '0.1.1';
-SELECT * FROM versions WHERE version !^ '0.1.1';
-
 SELECT * FROM versions WHERE version ^  '0.0.1';
+
+SELECT * FROM versions WHERE version !^ '1.1.1';
+SELECT * FROM versions WHERE version !^ '0.1.1';
 SELECT * FROM versions WHERE version !^ '0.0.1';
 
 DROP TABLE IF EXISTS versions;
